cc8960fde91794a4d1abbdb0c836e4ced2a776b4
myemcu <1142497680@qq.com>
2016-08-03 12:46:43 +0800

    V1，自定义Button实现的事件回调。

---
+++ app/src/main/java/com/myemcu/app_5callbackevent/MyButton.java
@@ -0,0 +1,29 @@
+package com.myemcu.app_5callbackevent;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.widget.Button;
+import android.widget.Toast;
+
+/**
+ * Created by Administrator on 2016/8/3.
+ */
+public class MyButton extends Button { //Alt+Enter 创建构造器(选择带两个参数的)
+
+    private Context context;
+
+    public MyButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        this.context=context;
+    }
+
+    @Override//Ctrl+O后敲onTouch即可找到
+    public boolean onTouchEvent(MotionEvent event) {
+
+        Toast.makeText(context,"MyButton中的触摸事件触发了!", Toast.LENGTH_SHORT).show();
+
+        return true;
+    }
+}
